04568606b1010e5bbf3aead0fb8624286c446c7b
Mathias Geat <mathias@ailoo.net>
2025-01-23 15:24:24 +0100

    Try try again..

--- entrypoint-action.sh
+++ entrypoint-action.sh
@@ -5,11 +5,17 @@ set -eu
 sh -c "git config --global --add safe.directory $PWD"
 
 result=$(git-that-semver "$@")
+json_result=$(git-that-semver "$@" -o json)
 
 # write to output
 echo "$result" | tee -a $GITHUB_OUTPUT
+echo "GTS_JSON=$json_result" | tee -a $GITHUB_OUTPUT
 
 # write to step summary
 echo '```env' >> $GITHUB_STEP_SUMMARY
 echo "$result" >> $GITHUB_STEP_SUMMARY
+echo '```' >> $GITHUB_STEP_SUMMARY
+
+echo '```json' >> $GITHUB_STEP_SUMMARY
+echo "$json_result" >> $GITHUB_STEP_SUMMARY
 echo '```' >> $GITHUB_STEP_SUMMARY
