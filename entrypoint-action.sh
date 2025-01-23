4ac8f0f3f2778e4350f374b6396d8a3dae39df7f
Mathias Geat <mathias@ailoo.net>
2025-01-23 13:49:18 +0100

    Revert "Try JSON format"
    This reverts commit 806b78fec0a47c19582f7ac3f62a876c2b18e4b3.

--- entrypoint-action.sh
+++ entrypoint-action.sh
@@ -4,6 +4,4 @@ set -eu
 
 sh -c "git config --global --add safe.directory $PWD"
 
-json_result=$(git-that-semver "$@" -o json)
-
-echo "gts_json_result=$json_result" | tee -a $GITHUB_OUTPUT
+git-that-semver "$@" | tee -a $GITHUB_OUTPUT
