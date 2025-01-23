9df4d2d5d342b62fd686f4e9c5bd5953d95e2038
Mathias Geat <mathias@ailoo.net>
2025-01-23 13:57:11 +0100

    Write job summary from entrypoint

--- entrypoint-action.sh
+++ entrypoint-action.sh
@@ -4,4 +4,15 @@ set -eu
 
 sh -c "git config --global --add safe.directory $PWD"
 
-git-that-semver "$@" | tee -a $GITHUB_OUTPUT
+echo "ENV START"
+env
+echo "ENV END"
+
+git-that-semver -c output.env.arrayDelimiter=, "$@" > /tmp/git-that-semver.env
+
+cat /tmp/git-that-semver.env | tee -a $GITHUB_OUTPUT
+
+echo '### git-that-semver result' >> $GITHUB_STEP_SUMMARY
+echo '```env' >> $GITHUB_STEP_SUMMARY
+cat /tmp/git-that-semver.env >> $GITHUB_STEP_SUMMARY
+echo '```' >> $GITHUB_STEP_SUMMARY
