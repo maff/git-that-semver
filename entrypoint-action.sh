9d63486982d1d7b8dad1cded3e1af656c6fa0dbc
Mathias Geat <mathias@ailoo.net>
2025-01-23 20:32:38 +0100

    Try newline handling

--- entrypoint-action.sh
+++ entrypoint-action.sh
@@ -5,11 +5,23 @@ set -eu
 sh -c "git config --global --add safe.directory $PWD"
 
 result=$(git-that-semver "$@")
-json_result=$(git-that-semver "$@" -o json)
+json_result=$(git-that-semver "$@" -o json -c output.json.indent=2)
+yaml_result=$(git-that-semver "$@" -o yaml)
 
 # write to output
 echo "$result" | tee -a $GITHUB_OUTPUT
-echo "GTS_JSON=$json_result" | tee -a $GITHUB_OUTPUT
+
+{
+    echo 'GTS_JSON<<EOF'
+    echo "$json_result"
+    echo EOF
+} >> $GITHUB_OUTPUT
+
+{
+    echo 'GTS_YAML<<EOF'
+    echo "$yaml_result"
+    echo EOF
+} >> $GITHUB_OUTPUT
 
 # write to step summary
 echo '```env' >> $GITHUB_STEP_SUMMARY
@@ -18,4 +30,8 @@ echo '```' >> $GITHUB_STEP_SUMMARY
 
 echo '```json' >> $GITHUB_STEP_SUMMARY
 echo "$json_result" >> $GITHUB_STEP_SUMMARY
+echo '```' >> $GITHUB_STEP_SUMMARY
+
+echo '```yaml' >> $GITHUB_STEP_SUMMARY
+echo "$yaml_result" >> $GITHUB_STEP_SUMMARY
 echo '```' >> $GITHUB_STEP_SUMMARY
